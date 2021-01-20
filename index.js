5438a604193bfb84a87d8a325cebfdd7f2ae18c5
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-20 23:42:00 +0900

    Implement:key.js를 이용한 비밀설정정보관리

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
-const POST = 5000;
-const dotenv = require("dotenv");
-dotenv.config();
+const config = require("./config/key");
 const mongoose = require("mongoose");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+
+const POST = 5000;
+//aplication/x-www-form-urlencoded분석
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json분석
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.USER}:${process.env.PW}@cluster0.isp9u.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`,
-    {
-      userNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => {
     console.log("Mongoodb Connect");
   })
@@ -21,6 +25,17 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  // user모델에 저장
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(POST, () => console.log(`http://localhost:5000`));
